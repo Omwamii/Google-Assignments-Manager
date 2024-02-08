fc41db382dd561ab13b9f2c6c89581c273c4c5d7
omwami <ianalpha@protonmail.com>
2024-02-08 23:17:43 +0300

    added views

--- assigno_manager/mysubs/views.py
+++ assigno_manager/mysubs/views.py
@@ -1,7 +1,19 @@
-from django.shortcuts import render
+from django.shortcuts import render_template
 
 
 
 def index(request):
     """ Index page """
-    return render('index.html')
+    return render_template('index.html')
+
+
+def submit(request):
+    """ Submit an assignent """
+
+
+def get_stats(request):
+    """ Get grades on assignmnents """
+
+
+def download_submission(request):
+    """ Download a submission had submitted """
