e92aecfaf225c07abdb0b84c190ed6c94bf05a16
omwami <ianalpha@protonmail.com>
2024-04-02 09:23:54 +0300

    change navbar 'home' title to 'pending work'

--- g-assigns/src/Navbar.js
+++ g-assigns/src/Navbar.js
@@ -3,15 +3,15 @@ import { Link } from 'react-router-dom';
 
 const Navbar = () => {
   return (
-    <nav className="navbar navbar-dark bg-dark fixed-top">
-      <div className="container-fluid d-flex">
-        <Link className="navbar-brand me-3" to="/">Home</Link>
-        <Link className="navbar-brand me-3" to="/notifications">Notifications</Link>
-        <Link className="navbar-brand" to='/grades'>Grades</Link>
-        <Link className="navbar-brand me-3" to='/stats'>Your progress</Link>
+    <nav className='navbar navbar-dark bg-dark fixed-top'>
+      <div className='container-fluid d-flex'>
+        <Link className='navbar-brand me-3' to='/'>Pending work</Link>
+        <Link className='navbar-brand me-3' to='/notifications'>Notifications</Link>
+        <Link className='navbar-brand' to='/grades'>Grades</Link>
+        <Link className='navbar-brand me-3' to='/stats'>Your progress</Link>
       </div>
     </nav>
   );
-}
+};
 
 export default Navbar;
