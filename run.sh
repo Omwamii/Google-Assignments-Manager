8f6589e713c7c995ce8606ccc58244e16b3e8235
omwami <ianalpha@protonmail.com>
2024-04-07 19:15:43 +0300

    edited run.sh to allow windows users to run with script

--- run.sh
+++ run.sh
@@ -8,10 +8,12 @@ pip3 install -r requirements.txt
 echo "Starting Gunicorn server..."
 if [ "$USE_CHART_DATA" = "REAL" ]; then
 	echo -e "\nUsing real student data to fetch stats...\n"
-	USE_CHART_DATA=REAL gunicorn assigno_manager.wsgi:application --timeout 60 &
+	# USE_CHART_DATA=REAL gunicorn assigno_manager.wsgi:application --timeout 60 &         -- windows machines no gunicorn
+	USE_CHART_DATA=REAL python3 manage.py runserver &
 else
 	echo -e "\nUsing dummy data to fetch stats...\n"
-	gunicorn assigno_manager.wsgi:application --timeout 60 &
+	# gunicorn assigno_manager.wsgi:application --timeout 60 &
+	python3 manage.py runserver &
 fi
 
 echo "Installing Node.js dependencies..."
